f1b4c442a3aeed0c552353c1114ff85a3f0abb92
Alihan-mb <66843425+Alihan-mb@users.noreply.github.com>
2022-02-28 14:21:52 +0400

    Delete Python directory

--- Python/TestATxt.py
+++
@@ -1,6 +0,0 @@
-with open('She.txt', 'r') as readear:
-    mama = readear.readlines()
-    reversed(mama)
-    with open('She.txt', 'w') as writer:
-        for papa in reversed(mama):
-            writer.write(papa)
