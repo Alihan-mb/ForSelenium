212cab6bcb8b7934b3911d2634c5d4878673a75d
Alihan-mb <66843425+Alihan-mb@users.noreply.github.com>
2022-02-28 14:21:24 +0400

    Delete PythonSelenium directory

--- PythonSelenium/OneMoreForLocators.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-mama = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
-mama.get("https://www.youtube.com/")
-mama.find_element_by_css_selector("input[name='search_query']").send_keys("Спартак")
-mama.find_element_by_id("search-icon-legacy").click()
